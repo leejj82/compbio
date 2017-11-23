c2dee3a06bbc56da8aaa27192c427b9cf64a9843
leejj82 <s179814@biohpcwsc034.biohpc.swmed.edu>
2017-11-23 16:10:45 -0600

    sdfs

--- lab1/hw12.py
+++ lab1/hw12.py
@@ -53,11 +53,6 @@ def split(arr, size):
     arrs.append(arr+[i+1])
     return arrs
 
-temp=[]
-     
-for i in range(0,1):
-    temp=split(seqs[i][1],10)
-
 def my_cmp(x, y):
     assert len(x) == len(y)
     for i in range(len(x)):
@@ -66,31 +61,18 @@ def my_cmp(x, y):
         elif x[i] > y[i]:
             return 1
     return 0
-        
-def cmp_to_key(mycmp):
-    'Convert a cmp= function into a key= function'
-    class K(object):
-        def __init__(self, obj, *args):
-            self.obj = obj
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-    return K
-    
-temp=sorted(temp, key=cmp_to_key(my_cmp))
 
-print(temp)
+temp=[]
+
+for i in range(0,1):
+    temp=split(seqs[i][1],10)
+    temp=sorted(temp, cmp=my_cmp)
+
+
+print temp
             
             
+
 
 
 
