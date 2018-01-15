039759d59527a6d32c373dde7607efa6a20801c0
leejj82 <s179814@biohpcwsc034.biohpc.swmed.edu>
2018-01-15 13:31:05 -0600

    .

--- lab3/hw31.py
+++ lab3/hw31.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys
 from datetime import datetime, date, time
 
@@ -9,6 +10,9 @@ print >> sys.stderr, "Begin at", str(datetime.now())
 min_ORF_len=200
 start_codons=['ATG','GTG']
 stop_codons=['TAA','TAG','TGA']
+
+len_start_codons=len(start_codons)
+len_stop_codons=len(stop_codons)
 
 def change_seq_to_num(seq):
     num=[]
@@ -33,6 +37,8 @@ for line in open(read_fname):
     line = line.strip()
     dna_seq_in_num.extend(change_seq_to_num(line))
 
+len_of_dna_seq=len(dna_seq_in_num)
+
 for codon in range(len(start_codons)):
     start_codons_in_num.append(change_seq_to_num(start_codons[codon]))
 
@@ -46,7 +52,32 @@ def reverse_complement(list):
         reverse_complement_list.append(3-list[len_of_list-i-1])
     return reverse_complement_list
 
-reversecomplement=reverse_complement(dna_seq_in_num)
+def compare_start_codons(list):
+    for i in range(len_start_codons):
+        if list==start_codons_in_num[i]:
+            return [1,i]
+    return [0,0]
+
+def compare_stop_codons(list):
+    for i in range(len_stop_codons):
+        if list==stop_codons_in_num[i]:
+            return [1,i]
+    return [0,0]    
+
+for i in range(len_of_dna_seq):
+    if compare_start_codons(dna_seq_in_num[i:i+3])[0]==1:
+        print i
+        
+
+
+    
+
+
+
+
+print start_codons_in_num
+print stop_codons_in_num
+
 
 
 
